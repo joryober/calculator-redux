64a2420189178bee7ddd764acd90ac8ecf6d280d
Jory Oberlies <joryoberlies@gmail.com>
2020-10-17 12:04:23 -0700

    working as intended, need to clean up with CSS

--- script.js
+++ script.js
@@ -49,40 +49,67 @@ numBtns.forEach((numBtn) => {
 let operBtns = document.querySelectorAll(".oper");
 operBtns.forEach((operBtn) => {
   operBtn.addEventListener("click", (e) => {
-    calculateValue += ` ${e.target.textContent} `;
-    displayValue = "";
+    calculateArr = calculateValue.split(" ");
+    if (calculateArr.length === 1) {
+      calculateValue += ` ${e.target.textContent} `;
+      displayValue = "";
+    } else if (calculateArr.length === 3) {
+      calculateArr[0] = +calculateArr[0];
+      calculateArr[2] = +calculateArr[2];
+
+      displayValue = operate(calculateArr[1], calculateArr[0], calculateArr[2]);
+
+      if (displayValue.toString().length > 15) {
+        displayValue =
+          Math.abs(displayValue) < 1
+            ? +displayValue.toFixed(13)
+            : +displayValue.toPrecision(14);
+      }
+
+      input.value = displayValue;
+      calculateValue = displayValue.toString();
+      calculateArr = [];
+      calculateValue += ` ${e.target.textContent} `;
+      displayValue = "";
+    }
   });
 });
 
 // EQUAL BUTTON CRASHES PROGRAM IF ONLY ONE VALUE ENTERED
 let equalBtn = document.querySelector(".eq");
 equalBtn.addEventListener("click", () => {
-  let decimalMultiplyCount = 0;
   calculateArr = calculateValue.split(" ");
-  if (calculateArr.length > 3) {
+  if (calculateArr[0] === "") return;
+  if (calculateArr.length === 1) {
     input.value = calculateArr[0];
+    displayValue = calculateArr[0];
+    calculateValue = calculateArr[0];
+  } else if (calculateArr.length === 2) {
+    input.value = "ERR";
     displayValue = "";
     calculateValue = "";
     calculateArr = [];
+  } else {
+    calculateArr[0] = +calculateArr[0];
+    calculateArr[2] = +calculateArr[2];
+
+    displayValue = operate(calculateArr[1], calculateArr[0], calculateArr[2]);
+
+    // input.value = displayValue;
+    // displayValue = "";
+    // calculateValue = "";
+    // calculateArr = [];
+    if (displayValue.toString().length > 15) {
+      displayValue =
+        Math.abs(displayValue) < 1
+          ? +displayValue.toFixed(13)
+          : +displayValue.toPrecision(14);
+    }
+
+    input.value = displayValue;
+    calculateValue = displayValue.toString();
+    calculateArr = [];
   }
-  calculateArr[0] = +calculateArr[0];
-  calculateArr[2] = +calculateArr[2];
-  while (
-    !Number.isInteger(calculateArr[0]) ||
-    !Number.isInteger(calculateArr[2])
-  ) {
-    calculateArr[0] *= 10;
-    calculateArr[2] *= 10;
-    decimalMultiplyCount++;
-  }
-  displayValue = operate(calculateArr[1], calculateArr[0], calculateArr[2]);
-  for (let i = 0; i < decimalMultiplyCount; i++) {
-    displayValue /= 10;
-  }
-  input.value = displayValue;
-  displayValue = "";
-  calculateValue = "";
-  calculateArr = [];
 });
 
 let clearBtn = document.querySelector(".clear");
@@ -90,4 +117,10 @@ clearBtn.addEventListener("click", () => {
   input.value = 0;
   displayValue = "";
   calculateValue = "";
+  calculateArr = [];
+});
+
+let decBtn = document.querySelector(".dec");
+decBtn.addEventListener("click", (e) => {
+  if (!displayValue.toString().includes(".")) populate(e.target.textContent);
 });
